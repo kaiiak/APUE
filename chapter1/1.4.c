0f74e9189984d374be1db7d8c89bbcf175bbab11
kaiiak <itkaikai@gmail.com>
2016-02-12 23:09:51 +0800

    1.4.c

---
+++ chapter1/1.4.c
@@ -0,0 +1,26 @@
+/*****将标准输入复制到标准输出******/
+#include "apue.h"
+
+#define BUFFSIZE 4096
+
+int
+mian(void)
+{
+	int		n;
+	char	buf[BUFFSIZE];
+
+	while ((n == read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+	{
+		if (write(STDIN_FILENO, buf, n) != n)
+		{
+			err_sys("write error");
+		}
+	}
+
+	if (n < 0)
+	{
+		err_sys("read error");
+	}
+
+	exit(0);
+}
