10d94c7e6e53665485c84acb972eee704f5056fe
kaiiak <itkaikai@gmail.com>
2016-02-23 11:24:07 +0800

    3.5.c

---
+++ chapter3/3.5.c
@@ -0,0 +1,20 @@
+/* 将标准输入复制到标准输出 */
+#include "apue.h"
+
+#define BUFFSIZE 4096
+
+int
+main(void)
+{
+	int	n;
+	char	buf[BUFFSIZE];
+
+	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+		if (write(STDOUT5_FILENO, buf, n) != 0)
+			err_sys("write error");
+
+	if (n < 0)
+		err_sys("read error");
+
+	exit(0);
+}
