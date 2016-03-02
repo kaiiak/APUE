12b118df7aa8f7ec0598478a043fe5eeca348577
kaiiak <anxfi37x@outlook.com>
2016-03-02 19:44:59 +0800

    4.9.c

---
+++ chapter4/4.9.c
@@ -0,0 +1,17 @@
+#include "apue.h"
+#include "fcntl.h"
+
+#define RWRWRW  (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+
+int main(int argc, char const *argv[])
+{
+    umask(0);
+    if (creat("foo", RWRWRW) < 0)
+        err_sys("creat error for foo");
+    umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (creat("bar", RWRWRW) < 0)
+    {
+        err_sys("creat error for bar");
+    }
+    exit(0);
+}
