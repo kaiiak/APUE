a8cd6d30b07e010127b815a24c8cc9900e97faf2
kaiiak <anxfi37x@outlook.com>
2016-06-05 22:40:40 +0800

    10.5.c

--- chapter10/10.5.c
+++ chapter10/10.5.c
@@ -16,7 +16,7 @@ main(void)
 	signal(SIGALRM, my_alarm);
 	alarm(1);
 	for ( ; ; ) {
-		if ((ptr = getpwnam("sar")) == NULL)
+		if ((ptr = getpwnam("kai")) == NULL)
 			err_sys("getpwnam error");
 		if (strcmp(ptr->pw_name, "sar") != 0)
 			printf("return value corrupted!, pw_name = %s\n",
